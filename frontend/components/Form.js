b9fee8f72dfff194c5a16de5c480113b781db04c
Sarah Pickering <sarahpsellsaz@gmail.com>
2024-09-20 18:15:15 -0700

    set up routing, navlinks, and home/form paths. Created form to submit pizza orders. Set up schema to validate fullName and size. Topping checkboxes optional

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import * as yup from 'yup'
+import axios from 'axios'
 
 // 👇 Here are the validation errors you will use with Yup.
 const validationErrors = {
@@ -9,6 +11,16 @@ const validationErrors = {
 
 // 👇 Here you will create your schema.
 
+const validationSchema = yup.object().shape({
+  fullName: yup.string()
+  .min(3, validationErrors.fullNameTooShort)
+    .max(20, validationErrors.fullNameTooLong)
+    .trim(),
+  size: yup.string()
+    .oneOf(["S", "M", "L"],
+        validationErrors.sizeIncorrect)
+})
+
 // 👇 This array could help you construct your checkboxes using .map in the JSX.
 const toppings = [
   { topping_id: '1', text: 'Pepperoni' },
@@ -18,44 +30,58 @@ const toppings = [
   { topping_id: '5', text: 'Ham' },
 ]
 
+const initialValues = { fullName: '', size: '', toppings: [] }
+
 export default function Form() {
+
+const [serverSuccess, setServerSuccess] = useState('')
+const [serverFailure, setServerFailure] = useState('')
+const [enabled, setEnabled] = useState(false)
+const [errors, setErrors] = useState({fullName: '', size: '' })
+const [values, setValues] = useState(initialValues)
+
   return (
     <form>
       <h2>Order Your Pizza</h2>
-      {true && <div className='success'>Thank you for your order!</div>}
-      {true && <div className='failure'>Something went wrong</div>}
+      {serverSuccess && <div className="success">{serverSuccess}</div>}
+      {serverFailure && <div className="failure">{serverFailure}</div>}
 
       <div className="input-group">
         <div>
-          <label htmlFor="fullName">Full Name</label><br />
+          <label htmlFor="fullName">Full Name</label>
+          <br />
           <input placeholder="Type full name" id="fullName" type="text" />
         </div>
-        {true && <div className='error'>Bad value</div>}
+        {true && <div className="error">Bad value</div>}
       </div>
 
       <div className="input-group">
         <div>
-          <label htmlFor="size">Size</label><br />
+          <label htmlFor="size">Size</label>
+          <br />
           <select id="size">
             <option value="">----Choose Size----</option>
-            {/* Fill out the missing options */}
+            <option value="S">Small</option>
+            <option value="M">Medium</option>
+            <option value="L">Large</option>
           </select>
         </div>
-        {true && <div className='error'>Bad value</div>}
+        {true && <div className="error">Bad value</div>}
       </div>
 
       <div className="input-group">
-        {/* 👇 Maybe you could generate the checkboxes dynamically */}
-        <label key="1">
-          <input
-            name="Pepperoni"
-            type="checkbox"
-          />
-          Pepperoni<br />
-        </label>
+        {toppings.map(({ topping_id, text }) => (
+          <label key={topping_id}>
+            <input name={topping_id} 
+            type="checkbox" />
+            { text }
+            <br />
+          </label>
+        ))}
+
       </div>
       {/* 👇 Make sure the submit stays disabled until the form validates! */}
       <input type="submit" />
     </form>
-  )
+  );
 }
--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import pizza from './images/pizza.jpg'
+import { useNavigate } from 'react-router-dom'
 
 function Home() {
+
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate('order')
+  }
+
   return (
     <div>
       <h2>
         Welcome to Bloom Pizza!
       </h2>
-      {/* clicking on the img should navigate to "/order" */}
-      <img alt="order-pizza" style={{ cursor: 'pointer' }} src={pizza} />
+      <img alt="order-pizza" style={{ cursor: 'pointer' }} 
+        src={pizza} onClick={handleClick} />
     </div>
   )
 }
